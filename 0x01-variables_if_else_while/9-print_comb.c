15eec9491260693819a8650c03233b1b0ae4c0a8
Kaburraaa <gochoyvonne@gmail.com>
2022-10-16 13:04:51 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,10 @@ int main(void)
 {
 	int n;
 
-	for (n = '0'; n <= '9'; n++)
+	for (n = '48'; n <= '58'; n++)
 	{
 		putchar(n);
-		if (n == 9)
-		{
-		break;
-		}
-		else
+		if (n != 57)
 		{
 			putchar(',');
 			putchar(' ');
