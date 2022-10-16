ec96dbe05d453608b92effbacda6b877803bbcc4
Kaburraaa <gochoyvonne@gmail.com>
2022-10-16 12:40:12 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of single digit
+ * main - prints all combination of single digits
  * Return: Always 0
  */
 int main(void)
@@ -14,7 +14,7 @@ int main(void)
 		if (n != 57)
 		{
 			putchar(',');
-			putchar(',');
+			putchar(' ');
 		}
 	putchar('\n');
 	return (0);
